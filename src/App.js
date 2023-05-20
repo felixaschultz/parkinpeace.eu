6ec447d0aee175c42fdd76f807e83cd514f5a0bb
Felix A. Schultz <felix.schultz@engapho.com>
2023-05-20 11:05:14 +0200

    Added footer and styleing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import "./App.css";
 export default function App(){
     return (<>
-        <h1>Parkinpeace.eu underdevelopment</h1>
-        <p>We are currently working on Parkinpeace.eu</p>
+        <main className="grid">
+            <h1>Under constraction</h1>
+            <p>We are currently working on Parkinpeace.eu</p>
+            <p>Something new and exiting is coming</p>
+            <footer>
+                <p>&copy; {new Date().getFullYear()} Parkinpeace.eu. All rights reserved.</p>
+                <p>Developer <a href="https://www.intastellarsolutions.com" target="_blank">Intastellar Solutions, International</a></p>
+            </footer>
+        </main>
     </>);
 }
