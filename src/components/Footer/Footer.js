ee2af40680ec1ea3c785d862c990d10e85d3e4aa
Felix A. Schultz <felix.schultz@engapho.com>
2023-05-20 10:36:41 +0200

    Added copyright to footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,9 +1,10 @@
 const { useState, useEffect, useRef } = React;
 import "./Footer.css";
 export default function Footer(){
+    const currentYear = new Date().getFullYear();
     return (<>
         <footer>
-
+            &copy; {currentYear} Parkinpeace. All rights reserved.
         </footer>
     </>);
 }
