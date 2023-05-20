e2b05e6b2c437c171fc5dccf1b59dea5d1f21b1b
Felix A. Schultz <felix.schultz@engapho.com>
2023-05-20 10:40:00 +0200

    Removed some settings

--- webpack.build.conf.js
+++ webpack.build.conf.js
@@ -8,16 +8,7 @@ const buildWebpackConfig = merge(baseWebpackConfig, {
   //!!!
   mode: 'production',
   plugins: [
-    new webpack.DefinePlugin({
-      'process.env': {
-        REACT_APP_API_FB_KEY: JSON.stringify(process.env.REACT_APP_API_FB_KEY),
-        REACT_APP_FB_AUTH_DOMAIN: JSON.stringify(process.env.REACT_APP_FB_AUTH_DOMAIN),
-        REACT_APP_FB_DATABASE_URL: JSON.stringify(process.env.REACT_APP_FB_DATABASE_URL),
-        REACT_APP_FB_PROJECT_ID: JSON.stringify(process.env.REACT_APP_FB_PROJECT_ID),
-        REACT_APP_STORAGE_BUCKET: JSON.stringify(process.env.REACT_APP_STORAGE_BUCKET),
-        REACT_APP_FB_MESSAGING_SENDER_ID: JSON.stringify(process.env.REACT_APP_FB_MESSAGING_SENDER_ID),
-      },
-    }),
+    
   ],
 });
 
